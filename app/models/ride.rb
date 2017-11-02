ddb36b1c72f246251c5c4c4e45e4e62d75ea6991
Jim Stricker <jmstricker93@gmail.com>
2017-11-02 10:52:47 -0400

    added business logic methods to Ride model

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,60 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+
+  belongs_to :user
+  belongs_to :attraction
+
+
+  def take_ride
+    #create array of response messages for rider
+    messages = [has_enough_tickets, is_tall_enough].compact
+    if !messages.empty?
+      #if messages array is not empty, add "Sorry." to beginning and join into one string.
+      messages.unshift("Sorry.").compact.join(" ")
+    else
+      #update ticket count, nausea, and happiness & save
+      self.user.tickets -= self.attraction.tickets
+      self.user.nausea += self.attraction.nausea_rating
+      self.user.happiness += self.attraction.happiness_rating
+      self.user.save  
+    end
+  end
+
+
+
+
+
+
+  def is_tall_enough
+    if user.height < attraction.min_height
+      "You are not tall enough to ride the #{self.attraction.name}."
+    end
+  end
+
+  def has_enough_tickets
+    if self.user_tickets < self.attraction_tickets
+      "You do not have enough tickets to ride the #{self.attraction.name}."
+    end
+  end
+
+  def has_no_nil
+    self&.user_tickets && self&.attraction_tickets && self&.user_height && self&.attraction_min_height
+  end
+
+
+  def user_tickets
+    self.user.tickets
+  end
+
+  def user_height
+    self.user.height
+  end
+
+  def attraction_tickets
+    self.attraction.tickets
+  end
+
+  def attraction_min_height
+    self.attraction.min_height
+  end
+
 end

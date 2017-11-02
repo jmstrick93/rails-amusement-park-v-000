5a63a2aa7d6d2db7828e4fa0f5ff73843638481f
Jim Stricker <jmstricker93@gmail.com>
2017-11-02 16:58:42 -0400

    flash messages and notices working properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] =  "User successfully created."
       redirect_to user_path(@user)
     else
-      flash[:error] = @user.errors.full_messages.to_sentence
+      flash[:error] = @user.errors.full_messages
       redirect_to new_user_path
+    end
+
+    def show
+      @user = User.find_by(id: params[:id])
     end
   end
 

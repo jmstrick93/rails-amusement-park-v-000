f381a80cdd6cead489a31086be2750df64e81fbf
Jim Stricker <jmstricker93@gmail.com>
2017-11-02 15:56:14 -0400

    get users#create working; activate flash messages; reformat users form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      flash[:error] = @user.errors.full_messages.to_sentence
+      redirect_to new_user_path
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets)
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def display_flash_messages
+    render partial: 'layouts/flash', :locals => {flash: flash}
+  end
+
 end

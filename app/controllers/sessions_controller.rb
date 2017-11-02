d0a79949274272430507daeeedd5b764a952d24e
Jim Stricker <jmstricker93@gmail.com>
2017-11-02 19:59:27 -0400

    created working signin link and stubbed out sessions controller,signin form

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,6 @@
+class SessionsController < ApplicationController
+
+  def sign_in
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   root 'welcome#home'
+  get '/signin', to: 'sessions#sign_in'
 
   resources :users
+
+
 
 end

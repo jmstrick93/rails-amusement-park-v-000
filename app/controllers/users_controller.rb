0925119848b492b660c7409885b1aa98bcfd02a1
Jim Stricker <jmstricker93@gmail.com>
2017-11-03 10:26:37 -0400

    added admin checkbox on signup form & admin indicator on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets)
+    params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets, :admin)
   end
 
 end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,13 @@
 module UserHelper
+  def show_if_admin(user)
+    if user.admin
+      content_tag(:p, content_tag(:em, '--ADMIN--'))
+    end
+  end
+
+  # def logout_button
+  #   if session[:user_id]
+  #     link_to "Log Out", logout_path
+  #   end
+  # end
 end

becce6d10d70cdbdbd3703ea573f51383e016ba2
Jim Stricker <jmstricker93@gmail.com>
2017-11-03 09:56:39 -0400

    added require_login filter & excluded some actions, wraped new user auth into private helper method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :require_login
+
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] ||= []
+      flash[:error] << "You must be logged in to access this section"
+      redirect_to root_path    # halts request cycle
+    end
+  end
+
+  def logged_in?
+    session[:user_id].present? && User.find_by(id: session[:user_id]) ? true : false
+  end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,15 @@
 class SessionsController < ApplicationController
 
+  skip_before_action :require_login, only: [:sign_in, :create]
+
   def sign_in
-    # binding.pry
+    #
     @user = User.new
-    # binding.pry
+    #
   end
 
   def create
-    @user = User.find_by(params[:user][:id])
-    if @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
-    else
-      flash = @user.try(:errors)
-      redirect_to signin_path
-    end
+    authenticate_user(@user)
   end
 
 
@@ -24,4 +19,15 @@ class SessionsController < ApplicationController
     params.require(:user).permit(:name, :password)
   end
 
+  def authenticate_user(user)
+    user = User.find_by(params[:user][:id])
+    if user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:errors] ||= []
+      flash[:errors]= user.errors if !!user.errors
+      redirect_to signin_path
+    end
+  end
 end
